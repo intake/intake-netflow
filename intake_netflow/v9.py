d9b960494c16185c8f71d77ab012a5450728f606
Joseph Crail <jbcrail@gmail.com>
2018-03-21 01:10:28 -0700

    Build struct object for template fields

--- intake_netflow/v9.py
+++ intake_netflow/v9.py
@@ -13,99 +13,103 @@ s_type_length = struct.Struct("!HH")
 
 
 class FieldType(enum.Enum):
-    IN_BYTES = 1
-    IN_PKTS = 2
-    FLOWS = 3
-    PROTOCOL = 4
-    SRC_TOS = 5
-    TCP_FLAGS = 6
-    L4_SRC_PORT = 7
-    IPV4_SRC_ADDR = 8
-    SRC_MASK = 9
-    INPUT_SNMP = 10
-    L4_DST_PORT = 11
-    IPV4_DST_ADDR = 12
-    DST_MASK = 13
-    OUTPUT_SNMP = 14
-    IPV4_NEXT_HOP = 15
-    SRC_AS = 16
-    DST_AS = 17
-    BGP_IPV4_NEXT_HOP = 18
-    MUL_DST_PKTS = 19
-    MUL_DST_BYTES = 20
-    LAST_SWITCHED = 21
-    FIRST_SWITCHED = 22
-    OUT_BYTES = 23
-    OUT_PKTS = 24
-    MIN_PKT_LNGTH = 25
-    MAX_PKT_LNGTH = 26
-    IPV6_SRC_ADDR = 27
-    IPV6_DST_ADDR = 28
-    IPV6_SRC_MASK = 29
-    IPV6_DST_MASK = 30
-    IPV6_FLOW_LABEL = 31
-    ICMP_TYPE = 32
-    MUL_IGMP_TYPE = 33
-    SAMPLING_INTERVAL = 34
-    SAMPLING_ALGORITHM = 35
-    FLOW_ACTIVE_TIMEOUT = 36
-    FLOW_INACTIVE_TIMEOUT = 37
-    ENGINE_TYPE = 38
-    ENGINE_ID = 39
-    TOTAL_BYTES_EXP = 40
-    TOTAL_PKTS_EXP = 41
-    TOTAL_FLOWS_EXP = 42
-    IPV4_SRC_PREFIX = 44
-    IPV4_DST_PREFIX = 45
-    MPLS_TOP_LABEL_TYPE = 46
-    MPLS_TOP_LABEL_IP_ADDR = 47
-    FLOW_SAMPLER_ID = 48
-    FLOW_SAMPLER_MODE = 49
-    FLOW_SAMPLER_RANDOM_INTERVAL = 50
-    MIN_TTL = 52
-    MAX_TTL = 53
-    IPV4_IDENT = 54
-    DST_TOS = 55
-    IN_SRC_MAC = 56
-    OUT_DST_MAC = 57
-    SRC_VLAN = 58
-    DST_VLAN = 59
-    IP_PROTOCOL_VERSION = 60
-    DIRECTION = 61
-    IPV6_NEXT_HOP = 62
-    BPG_IPV6_NEXT_HOP = 63
-    IPV6_OPTION_HEADERS = 64
-    MPLS_LABEL_1 = 70
-    MPLS_LABEL_2 = 71
-    MPLS_LABEL_3 = 72
-    MPLS_LABEL_4 = 73
-    MPLS_LABEL_5 = 74
-    MPLS_LABEL_6 = 75
-    MPLS_LABEL_7 = 76
-    MPLS_LABEL_8 = 77
-    MPLS_LABEL_9 = 78
-    MPLS_LABEL_10 = 79
-    IN_DST_MAC = 80
-    OUT_SRC_MAC = 81
-    IF_NAME = 82
-    IF_DESC = 83
-    SAMPLER_NAME = 84
-    IN_PERMANENT_BYTES = 85
-    IN_PERMANENT_PKTS = 86
-    FRAGMENT_OFFSET = 88
-    FORWARDING_STATUS = 89
-    MPLS_PAL_RD = 90
-    MPLS_PREFIX_LEN = 91
-    SRC_TRAFFIC_INDEX = 92
-    DST_TRAFFIC_INDEX = 93
-    APPLICATION_DESCRIPTION = 94
-    APPLICATION_TAG = 95
-    APPLICATION_NAME = 96
-    POST_IP_DIFF_SERV_CODE_POINT = 98
-    REPLICATION_FACTOR = 99
-    LAYER2_PACKET_SECTION_OFFSET = 102
-    LAYER2_PACKET_SECTION_SIZE = 103
-    LAYER2_PACKET_SECTION_DATA = 104
+    IN_BYTES = (1, int)
+    IN_PKTS = (2, int)
+    FLOWS = (3, int)
+    PROTOCOL = (4, int)
+    SRC_TOS = (5, int)
+    TCP_FLAGS = (6, int)
+    L4_SRC_PORT = (7, int)
+    IPV4_SRC_ADDR = (8, int)
+    SRC_MASK = (9, int)
+    INPUT_SNMP = (10, int)
+    L4_DST_PORT = (11, int)
+    IPV4_DST_ADDR = (12, int)
+    DST_MASK = (13, int)
+    OUTPUT_SNMP = (14, int)
+    IPV4_NEXT_HOP = (15, int)
+    SRC_AS = (16, int)
+    DST_AS = (17, int)
+    BGP_IPV4_NEXT_HOP = (18, int)
+    MUL_DST_PKTS = (19, int)
+    MUL_DST_BYTES = (20, int)
+    LAST_SWITCHED = (21, int)
+    FIRST_SWITCHED = (22, int)
+    OUT_BYTES = (23, int)
+    OUT_PKTS = (24, int)
+    MIN_PKT_LNGTH = (25, int)
+    MAX_PKT_LNGTH = (26, int)
+    IPV6_SRC_ADDR = (27, bytes)
+    IPV6_DST_ADDR = (28, bytes)
+    IPV6_SRC_MASK = (29, int)
+    IPV6_DST_MASK = (30, int)
+    IPV6_FLOW_LABEL = (31, bytes)
+    ICMP_TYPE = (32, int)
+    MUL_IGMP_TYPE = (33, int)
+    SAMPLING_INTERVAL = (34, int)
+    SAMPLING_ALGORITHM = (35, int)
+    FLOW_ACTIVE_TIMEOUT = (36, int)
+    FLOW_INACTIVE_TIMEOUT = (37, int)
+    ENGINE_TYPE = (38, int)
+    ENGINE_ID = (39, int)
+    TOTAL_BYTES_EXP = (40, int)
+    TOTAL_PKTS_EXP = (41, int)
+    TOTAL_FLOWS_EXP = (42, int)
+    IPV4_SRC_PREFIX = (44, int)
+    IPV4_DST_PREFIX = (45, int)
+    MPLS_TOP_LABEL_TYPE = (46, int)
+    MPLS_TOP_LABEL_IP_ADDR = (47, int)
+    FLOW_SAMPLER_ID = (48, int)
+    FLOW_SAMPLER_MODE = (49, int)
+    FLOW_SAMPLER_RANDOM_INTERVAL = (50, int)
+    MIN_TTL = (52, int)
+    MAX_TTL = (53, int)
+    IPV4_IDENT = (54, int)
+    DST_TOS = (55, int)
+    IN_SRC_MAC = (56, bytes)
+    OUT_DST_MAC = (57, bytes)
+    SRC_VLAN = (58, int)
+    DST_VLAN = (59, int)
+    IP_PROTOCOL_VERSION = (60, int)
+    DIRECTION = (61, int)
+    IPV6_NEXT_HOP = (62, bytes)
+    BPG_IPV6_NEXT_HOP = (63, bytes)
+    IPV6_OPTION_HEADERS = (64, int)
+    MPLS_LABEL_1 = (70, bytes)
+    MPLS_LABEL_2 = (71, bytes)
+    MPLS_LABEL_3 = (72, bytes)
+    MPLS_LABEL_4 = (73, bytes)
+    MPLS_LABEL_5 = (74, bytes)
+    MPLS_LABEL_6 = (75, bytes)
+    MPLS_LABEL_7 = (76, bytes)
+    MPLS_LABEL_8 = (77, bytes)
+    MPLS_LABEL_9 = (78, bytes)
+    MPLS_LABEL_10 = (79, bytes)
+    IN_DST_MAC = (80, bytes)
+    OUT_SRC_MAC = (81, bytes)
+    IF_NAME = (82, str)
+    IF_DESC = (83, str)
+    SAMPLER_NAME = (84, str)
+    IN_PERMANENT_BYTES = (85, int)
+    IN_PERMANENT_PKTS = (86, int)
+    FRAGMENT_OFFSET = (88, int)
+    FORWARDING_STATUS = (89, int)
+    MPLS_PAL_RD = (90, bytes)
+    MPLS_PREFIX_LEN = (91, int)
+    SRC_TRAFFIC_INDEX = (92, int)
+    DST_TRAFFIC_INDEX = (93, int)
+    APPLICATION_DESCRIPTION = (94, str)
+    APPLICATION_TAG = (95, bytes)
+    APPLICATION_NAME = (96, str)
+    POST_IP_DIFF_SERV_CODE_POINT = (98, int)
+    REPLICATION_FACTOR = (99, int)
+    LAYER2_PACKET_SECTION_OFFSET = (102, int)
+    LAYER2_PACKET_SECTION_SIZE = (103, int)
+    LAYER2_PACKET_SECTION_DATA = (104, bytes)
+
+    def __init__(self, id, dtype):
+        self.id = id
+        self.dtype = dtype
 
 
 @attr.s
@@ -144,6 +148,27 @@ def decode_flowset(source):
     raise Exception("unknown flowset id '{}'".format(flowset_id))
 
 
+def create_struct(dtype, length):
+    if dtype is int:
+        if length == 1:
+            code = 'B'
+        elif length == 2:
+            code = 'H'
+        elif length == 4:
+            code = 'I'
+        elif length == 8:
+            code = 'Q'
+        else:
+            raise TypeError("invalid integer length: {}".format(length))
+    elif dtype is bytes:
+        code = "B" * length
+    elif dtype is str:
+        code = "{}s".format(length)
+    else:
+        raise TypeError("invalid datatype: {}".format(dtype))
+    return struct.Struct('!' + code)
+
+
 @attr.s
 class TemplateField(object):
     type = attr.ib(type=FieldType)
@@ -152,7 +177,7 @@ class TemplateField(object):
     @property
     def struct(self):
         if not hasattr(self, 'struct'):
-            self.struct = struct.Struct('!' + 'B' * self.length)
+            self.struct = create_struct(self.type.dtype, self.length)
         return self.struct
 
     @staticmethod
