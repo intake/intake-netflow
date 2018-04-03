dc5291a81c04955ce10673b5be8da68619be0159
Joseph Crail <jbcrail@gmail.com>
2018-04-03 12:20:58 -0700

    Add documentation

--- intake_netflow/utils.py
+++ intake_netflow/utils.py
@@ -2,7 +2,14 @@ import io
 
 
 def read_and_unpack(source, obj):
-    """Read and deserialize structure from stream."""
+    """Read and deserialize structure from stream.
+    
+    Parameters:
+        source : io.BufferedReader
+            Read-only input stream.
+        obj : struct.Struct
+            Deserialization struct.
+    """
     return obj.unpack(source.read(obj.size))
 
 
