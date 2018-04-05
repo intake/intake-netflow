a1c6cf06468aa6a47df0555ce63736c142f96fac
Joseph Crail <jbcrail@gmail.com>
2018-04-05 15:27:40 -0700

    Add tests for data source

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     maintainer_email='jbcrail@gmail.com',
     license='BSD',
     packages=find_packages(),
-    package_data={'': ['*.yml', '*.html']},
+    package_data={'': ['*.netflow', '*.yml', '*.html']},
     include_package_data=True,
     install_requires=requires,
     long_description=open('README.md').read(),
